fd36b61e33c39cf1757432cd0a81b905788073a7
thinker227 <thinker0227@gmail.com>
2022-11-05 16:06:59 +0100

    Move tool scripts

--- install-tool.ps1
+++ install-tool.ps1
@@ -3,4 +3,4 @@ Write-Output "Building and installing tool..."
 dotnet restore
 dotnet build --no-restore
 dotnet pack --no-build
-dotnet tool install --global --add-source ./bin/package FolderTool --prerelease
+dotnet tool install --global --add-source ./src/FolderTool/bin/package FolderTool --prerelease
--- update-tool.ps1
+++ update-tool.ps1
@@ -3,4 +3,4 @@ Write-Output "Building and updating tool..."
 dotnet restore
 dotnet build --no-restore
 dotnet pack --no-build
-dotnet tool update --global --add-source ./bin/package FolderTool --prerelease
+dotnet tool update --global --add-source ./src/FolderTool/bin/package FolderTool --prerelease
