dbbe8f8530fc633290870a2074d630db250f5d7d
thinker227 <thinker0227@gmail.com>
2022-11-06 10:44:56 +0100

    Update tool scripts with Tool configuration

--- install-tool.ps1
+++ install-tool.ps1
@@ -1,6 +1,6 @@
 Write-Output "Building and installing tool..."
 
 dotnet restore
-dotnet build --no-restore
+dotnet build --no-restore --configuration Tool
 dotnet pack --no-build
 dotnet tool install --global --add-source ./src/FolderTool/bin/package FolderTool --prerelease
--- update-tool.ps1
+++ update-tool.ps1
@@ -1,6 +1,6 @@
 Write-Output "Building and updating tool..."
 
 dotnet restore
-dotnet build --no-restore
+dotnet build --no-restore --configuration Tool
 dotnet pack --no-build
 dotnet tool update --global --add-source ./src/FolderTool/bin/package FolderTool --prerelease
