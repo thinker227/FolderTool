162fc86fde72f52359a6516c25a8ab7219687b7a
thinker227 <thinker0227@gmail.com>
2022-11-06 11:15:36 +0100

    Add error handling stuff

--- install-tool.ps1
+++ install-tool.ps1
@@ -1,6 +1,20 @@
+function Invoke-With-Exit {
+	param (
+		[scriptblock] $Action
+	)
+	
+	Write-Output "Running '$Action'..."
+
+	$Action.Invoke()
+
+	if ($LASTEXITCODE -ne 0) {
+		Exit 1
+	}
+}
+
 Write-Output "Building and installing tool..."
 
-dotnet restore
-dotnet build --no-restore --configuration Tool
-dotnet pack --no-build
-dotnet tool install --global --add-source ./src/FolderTool/bin/package FolderTool --prerelease
+Invoke-With-Exit {dotnet restore}
+Invoke-With-Exit {dotnet build --no-restore --configuration Tool}
+Invoke-With-Exit {dotnet pack --no-build}
+Invoke-With-Exit {dotnet tool install --global --add-source ./src/FolderTool/bin/package FolderTool --prerelease}
