be830d75a28cd0b80c6cbf3ad9270e0e5c0cbdfc
thinker227 <thinker0227@gmail.com>
2022-11-06 10:35:19 +0100

    Use cache for deserialized icons

--- src/FolderTool/Rendering/Icons/Icon.cs
+++ src/FolderTool/Rendering/Icons/Icon.cs
@@ -10,14 +10,18 @@ public static class Icon
     private const string fileIconsResourceLocation = "FolderTool.Resources.FileIcons.yaml";
     private const string directoryIconsResourceLocation = "FolderTool.Resources.DirectoryIcons.yaml";
 
+    private static FileIconDefinitionsModel? fileIconDefinitions = null;
+    private static DirectoryIconDefinitionsModel? directoryIconDefinitions = null;
+
     private static readonly IDeserializer deserializer = new DeserializerBuilder()
         .WithNamingConvention(HyphenatedNamingConvention.Instance)
         .Build();
 
+
+
     public static string GetFileIcon(FileInfo file)
     {
-        string resource = ManifestResourceHelper.ReadResource(fileIconsResourceLocation);
-        var icons = deserializer.Deserialize<FileIconDefinitionsModel>(resource);
+        var icons = GetFileIconDefinitions();
 
         string extension = string.IsNullOrEmpty(file.Extension)
             ? ""
@@ -30,13 +34,40 @@ public static class Icon
 
     public static string GetDirectoryIcon(DirectoryInfo directory)
     {
-        string resource = ManifestResourceHelper.ReadResource(directoryIconsResourceLocation);
-        var icons = deserializer.Deserialize<DirectoryIconDefinitionsModel>(resource);
+        var icons = GetDirectoryIconDefinitions();
 
         string name = directory.Name;
         return icons.Icons
             .FirstOrDefault(icon => icon.GetNames().Contains(name))
             ?.Icon
             ?? icons.Default;
+    }
+
+    private static FileIconDefinitionsModel GetFileIconDefinitions()
+    {
+        if (fileIconDefinitions is not null)
+        {
+            return fileIconDefinitions;
+        }
+
+        string resource = ManifestResourceHelper.ReadResource(fileIconsResourceLocation);
+        var icons = deserializer.Deserialize<FileIconDefinitionsModel>(resource);
+
+        fileIconDefinitions = icons;
+        return icons;
+    }
+
+    private static DirectoryIconDefinitionsModel GetDirectoryIconDefinitions()
+    {
+        if (directoryIconDefinitions is not null)
+        {
+            return directoryIconDefinitions;
+        }
+
+        string resource = ManifestResourceHelper.ReadResource(directoryIconsResourceLocation);
+        var icons = deserializer.Deserialize<DirectoryIconDefinitionsModel>(resource);
+
+        directoryIconDefinitions = icons;
+        return icons;
     }
 }
